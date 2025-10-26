7d0740e95205bf462f0db12c4167670b1c92c565
hyujikoh <dvum0045@gmail.com>
2025-10-27 00:37:13 +0900

    refactor : 사용자 등록 요청 및 엔티티 검증 로직 추가

--- apps/commerce-api/src/main/java/com/loopers/domain/user/UserEntity.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/user/UserEntity.java
@@ -8,6 +8,7 @@ import lombok.*;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.Valid;
 
 /**
  * @author hyunjikoh
@@ -26,11 +27,31 @@ public class UserEntity extends BaseEntity {
 
     private LocalDate birthdate;
 
-    public static UserEntity createUserEntity(UserRegisterRequest request) {
+    public static UserEntity createUserEntity(@Valid UserRegisterRequest request) {
+        if (request.username() == null || !request.username().matches("^[A-Za-z0-9]{1,10}$")) {
+            throw new IllegalArgumentException("사용자명은 영문 및 숫자 10자 이내여야 합니다.");
+        }
+
+        if (request.email() == null || !request.email().matches("^[A-Za-z0-9+_.-]+@(.+)$")) {
+            throw new IllegalArgumentException("올바른 형식의 이메일 주소여야 합니다.");
+        }
+
+        try {
+            if (request.birthdate() == null) {
+                throw new IllegalArgumentException("생년월일은 필수 입력값입니다.");
+            }
+            LocalDate.parse(request.birthdate());
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e instanceof IllegalArgumentException ?
+                    e.getMessage() : "올바른 날짜 형식이어야 합니다.");
+        }
+
         return UserEntity.builder()
                 .username(request.username())
                 .email(request.email())
                 .birthdate(LocalDate.parse(request.birthdate()))
                 .build();
     }
+
+
 }
--- apps/commerce-api/src/main/java/com/loopers/domain/user/UserRegisterRequest.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/user/UserRegisterRequest.java
@@ -1,14 +1,12 @@
 package com.loopers.domain.user;
 
-import java.time.LocalDate;
-
 /**
  * @author hyunjikoh
  * @since 2025. 10. 26.
  */
 public record UserRegisterRequest(
-    String username,
-    String email,
-    String birthdate
+        String username,
+        String email,
+        String birthdate
 ) {
 }
