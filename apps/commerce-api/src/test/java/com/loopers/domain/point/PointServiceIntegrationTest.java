ff1b6ff58579c4e3a71bcef8823cfc0f3175cdd3
hyujikoh <dvum0045@gmail.com>
2025-10-29 20:40:27 +0900

    test : 사용자 정보 유무에 따른 포인트 정보 조회 테스트 코드 작성

--- apps/commerce-api/src/test/java/com/loopers/domain/point/PointServiceIntegrationTest.java
+++ apps/commerce-api/src/test/java/com/loopers/domain/point/PointServiceIntegrationTest.java
@@ -1,55 +1,68 @@
 package com.loopers.domain.point;
 
-import static org.mockito.Mockito.when;
-
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.Extensions;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-import com.loopers.domain.user.*;
+import com.loopers.application.user.UserFacade;
+import com.loopers.application.user.UserInfo;
+import com.loopers.domain.user.Gender;
+import com.loopers.domain.user.UserRegisterRequest;
+import com.loopers.domain.user.UserRepository;
+import com.loopers.utils.DatabaseCleanUp;
 
 /**
  * @author hyunjikoh
  * @since 2025. 10. 29.
  */
 @SpringBootTest
-@ExtendWith(MockitoExtension.class)
 public class PointServiceIntegrationTest {
-    @Autowired
-    private UserService userService;
 
     @Autowired
     private UserRepository userRepository;
 
-    @MockitoBean
+    @Autowired
     private PointService pointService;
 
+    @Autowired
+    private DatabaseCleanUp databaseCleanUp;
 
+    @Autowired
+    private UserFacade userFacade;
 
+    @AfterEach
+    void tearDown() {
+        databaseCleanUp.truncateAllTables();
+    }
 
     @org.junit.jupiter.api.Test
     @DisplayName("등록된 사용자의 포인트 조회를 성공한다.")
     void get_exist_user_point_amount() {
         // given
         UserRegisterRequest request = createUserRegisterRequest("testuser", "existing@email.com", "1990-01-01");
-        UserEntity save = userRepository.save(UserEntity.createUserEntity(request));
+        UserInfo userInfo = userFacade.registerUser(request);
 
-        Point pointEntity = Point.createPointEntity(save);
-        when(pointService.getByUser(save)).thenReturn(pointEntity);
         // when
-        Point point = pointService.getByUser(save);
+        PointEntity point = pointService.getByUsername(userInfo.username());
 
         // then
         Assertions.assertThat(point.getAmount()).isNotNull();
-        Assertions.assertThat(point.getUser()).isEqualTo(save);
+        Assertions.assertThat(point.getUser().getId()).isEqualTo(userInfo.id());
         Assertions.assertThat(point.getAmount()).isNotNegative();
-
     }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("해당 ID 의 회원이 존재하지 않을 경우, null 이 반환된다.")
+    void no_exist_user_null() {
+        // when
+        PointEntity point = pointService.getByUsername("nonexistentuser");
+
+        // then
+        Assertions.assertThat(point).isNull();
+    }
+
     private UserRegisterRequest createUserRegisterRequest(String username, String email, String birthdate) {
         return new UserRegisterRequest(username, email, birthdate, Gender.MALE);
     }
